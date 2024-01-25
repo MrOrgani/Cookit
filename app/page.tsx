56fa185adec39b9f3a762ee42d7baf9fc0ccf357
Maxime Organi <maxime.organi@gmail.com>
2024-01-25 15:07:12 +0100

    Refactor page layout and font styles

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,13 @@ import { UrlForm } from "./_components/url-form";
 
 export default function Home() {
 	return (
-		<main className="p-4 min-h-screen items-center h-screen justify-center w-full">
+		<main className="p-4 min-h-screen items-center h-screen justify-center w-full ">
 			<div className="z-10 w-full items-center justify-between font-mono text-sm flex flex-col h-full">
-				<h1 className="flex flex-col">
-					<span className={"text-4xl font-bold font-fredoka text-black"}>
+				<h1 className="flex flex-col font-montserrat text-4xl text-black">
+					<span className={""}>
 						FIND IT.<br></br>PASTE IT.
 					</span>
-					<span className={"text-4xl font-bold font-fredoka text-black"}>
-						COOK IT.
-					</span>
+					<span className={""}>COOK IT.</span>
 				</h1>
 				<UrlForm />
 			</div>
