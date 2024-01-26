5c4589b3ba0c15acd14a4b0b945a79f0416d83d6
Maxime Organi <maxime.organi@gmail.com>
2024-01-26 19:11:04 +0100

    using next/font for montserrat

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
+import { Montserrat } from "next/font/google";
 import { UrlForm } from "./_components/url-form";
+
+const montserrat = Montserrat({ subsets: ["latin"], weight: ["800"] });
 
 export default function Home() {
 	return (
 		<main className="p-4 min-h-screen items-center h-screen justify-center w-full ">
 			<div className="z-10 w-full items-center justify-between font-mono text-sm flex flex-col h-full">
-				<h1 className="flex flex-col font-montserrat text-4xl text-black">
+				<h1
+					className={`flex flex-col ${montserrat.className} text-4xl text-black`}
+				>
 					<span className={""}>
 						FIND IT.<br></br>PASTE IT.
 					</span>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,9 +18,6 @@ const config = {
 			},
 		},
 		extend: {
-			fontFamily: {
-				montserrat: ["Montserrat", "sans-serif"],
-			},
 			colors: {
 				border: "hsl(var(--border))",
 				input: "hsl(var(--input))",
