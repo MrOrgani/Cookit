a10a80db1b93274582d90cdbb352859c1f238af4
Maxime Organi <maxime.organi@gmail.com>
2024-01-31 15:55:05 +0100

    Created Setting Component

---
+++ app/_components/settings.tsx
@@ -0,0 +1,32 @@
+"use client";
+import { SettingsIcon } from "lucide-react";
+
+import {
+	Popover,
+	PopoverContent,
+	PopoverTrigger,
+} from "@/components/ui/popover";
+import { useUserPreferences } from "@/hooks/use-user-preferences";
+import { ApikeyInput } from "./apikey-input";
+import { LanguageSelector } from "./language-selector";
+import { UnitSelector } from "./unit-selector";
+
+export const Settings = () => {
+	const openSettings = useUserPreferences((state) => state.openSettings);
+	const setOpenSettings = useUserPreferences((state) => state.setOpenSettings);
+	return (
+		<Popover
+			open={openSettings}
+			onOpenChange={(opened) => setOpenSettings(opened)}
+		>
+			<PopoverTrigger className="fixed right-2 top-2">
+				<SettingsIcon className="  hover:animate-spin" />
+			</PopoverTrigger>
+			<PopoverContent className="space-y-4">
+				<ApikeyInput />
+				<LanguageSelector />
+				<UnitSelector />
+			</PopoverContent>
+		</Popover>
+	);
+};
