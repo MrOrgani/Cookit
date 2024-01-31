e4b363150d77f6e22ec07d67f0e831f6cbaf0123
Maxime Organi <maxime.organi@gmail.com>
2024-01-31 15:51:18 +0100

    moving components to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,14 @@
 import type { Metadata } from "next";
+import { Montserrat } from "next/font/google";
 import { Toaster } from "sonner";
+import { Settings } from "./_components/settings";
 import "./globals.css";
 
 export const metadata: Metadata = {
 	title: "Cookit",
 	description: "Translate your recipes to your language",
 };
+const montserrat = Montserrat({ subsets: ["latin"], weight: ["800"] });
 
 export default function RootLayout({
 	children,
@@ -16,8 +19,23 @@ export default function RootLayout({
 		<html lang="en">
 			<head />
 			<body>
-				<main>{children}</main>
 				<Toaster />
+				<div>
+					<Settings />
+				</div>
+				<main className="p-4 min-h-screen items-center h-screen justify-center w-full ">
+					<div className="z-10 w-full items-center justify-between font-mono text-sm flex flex-col h-full">
+						<h1
+							className={`flex flex-col ${montserrat.className} text-4xl text-black`}
+						>
+							<span className={""}>
+								FIND IT.<br></br>PASTE IT.
+							</span>
+							<span className={""}>COOK IT.</span>
+						</h1>
+						{children}
+					</div>
+				</main>
 			</body>
 		</html>
 	);
--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,18 @@
-import { Montserrat } from "next/font/google";
 import { CopyImageToClipboardButton } from "./_components/copy-to-clipboard-button";
 import { DownloadImageButton } from "./_components/download-image-button";
 import { RecipeDisplay } from "./_components/recipe-display";
 import { StreamingLoader } from "./_components/streaming-loader";
 import { UrlForm } from "./_components/url-form";
 
-const montserrat = Montserrat({ subsets: ["latin"], weight: ["800"] });
-
 export default function Home() {
 	return (
-		<main className="p-4 min-h-screen items-center h-screen justify-center w-full ">
-			<div className="z-10 w-full items-center justify-between font-mono text-sm flex flex-col h-full">
-				<h1
-					className={`flex flex-col ${montserrat.className} text-4xl text-black`}
-				>
-					<span className={""}>
-						FIND IT.<br></br>PASTE IT.
-					</span>
-					<span className={""}>COOK IT.</span>
-				</h1>
-				<UrlForm>
-					<CopyImageToClipboardButton />
-					<DownloadImageButton />
-				</UrlForm>
-				<StreamingLoader />
-				<RecipeDisplay />
-			</div>
-		</main>
+		<>
+			<UrlForm>
+				<CopyImageToClipboardButton />
+				<DownloadImageButton />
+			</UrlForm>
+			<StreamingLoader />
+			<RecipeDisplay />
+		</>
 	);
 }
